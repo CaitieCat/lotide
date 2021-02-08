29f2a6b7af5504e2ef9eda5d519d71d2435c8850
Caitie Garrood <caitlinanneg@gmail.com>
2021-02-08 13:08:36 -0500

    assertEqual updated

--- assertEqual.js
+++ assertEqual.js
@@ -5,6 +5,5 @@ const assertEqual = function(actual, expected) {
     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-assertEqual('Lighthouse Labs', 'Lighthouse Labs  ');
-assertEqual(1, 1.1);
-assertEqual(1, 1);
+
+module.exports = assertEqual;
