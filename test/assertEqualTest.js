f215934cd069e6284f146bf9fc018c9fbebd224d
Caitie Garrood <caitlinanneg@gmail.com>
2021-02-08 14:51:35 -0500

    assertEqualTest with Mocha

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,5 +1,15 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 
-assertEqual('Lighthouse Labs', 'Lighthouse Labs  ');
-assertEqual(1, 1.1);
-assertEqual(1, 1);
+describe("#assertEqual", () => {
+    it("returns false for Lighthouse Labs and Lighthouse Labs ", () => {
+      assert.notDeepEqual(assertEqual('Lighthouse Labs', 'Lighthouse Labs '), true);
+    });
+    it("returns false for 1 and 1.1 ", () => {
+        assert.notDeepEqual(assertEqual(1 , 1.1), true);
+      });
+    it("returns true for 1 and 1 ", () => {
+        assert.notDeepEqual(assertEqual(1 , 1), false);
+    });
+
+});
