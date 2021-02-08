9abb31f12126b9cef89391c160ed29ccceb75453
Caitie Garrood <caitlinanneg@gmail.com>
2021-02-08 14:14:17 -0500

    middle Updated

--- middle.js
+++ middle.js
@@ -1,28 +1,6 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-    let result = false;
-    if (arrayOne.length === arrayTwo.length) {
-      for (let i = 0; i < arrayOne.length; i++) {
-        if (arrayOne[i] === arrayTwo[i]) {
-          result = true;
-        }
-        if (arrayOne[i] !== arrayTwo[i]) {
-          result = false;
-        }
-      }
-    } else {
-      result = false;
-    }
-    return result;
-  };
-  
-  const assertArraysEqual = function(oneArray, twoArray) {
-    if (eqArrays(oneArray, twoArray) === true) {
-      console.log('✅✅✅ Assertion Passed');
-    } else {
-      console.log('❌❌❌ Assertion Failed');
-    }
-  };
-  assertArraysEqual([1, 2, 3], [1, 2, 3]);
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+
 
   const middle = function(list){
       if(list.length % 2 === 0) {
@@ -34,4 +12,5 @@ const eqArrays = function(arrayOne, arrayTwo) {
       }
       console.log(list);
   }
-  middle([1, 2, 3, 4, 5, 6, 7]);
+
+  module.exports = middle;
