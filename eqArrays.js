86b10fb7bf2bfdaf65c2d2d4ec02998afa80d1cb
Caitie Garrood <caitlinanneg@gmail.com>
2021-02-08 14:02:40 -0500

    eqArrays updated

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,3 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
-  /*assertEqual('Lighthouse Labs', 'Lighthouse Labs  ');
-  assertEqual(1, 1.1);
-  assertEqual(1, 1);*/
-
   const eqArrays = function(arrayOne, arrayTwo){
     let result = false;
     if (arrayOne.length === arrayTwo.length){  
@@ -27,4 +16,4 @@ const assertEqual = function(actual, expected) {
     return result;  
   }
 
-  assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true);
+  module.exports = eqArrays;
