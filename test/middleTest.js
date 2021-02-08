645ee81e7f43d9a9b9093f825ee5237142e72fe8
Caitie Garrood <caitlinanneg@gmail.com>
2021-02-08 15:08:28 -0500

    middleTest with Mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,3 +1,10 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-middle([1, 2, 3, 5, 6, 7, 8]);
+describe("#middle", () => {
+    it("returns 5 for [1, 2, 3, 5, 6, 7, 8]", () => {
+      assert.deepStrictEqual(middle([1, 2, 3, 5, 6, 7, 8]), [5]);
+    });
+});
+
+//middle([1, 2, 3, 5, 6, 7, 8]);
