cc51533709e0dd70eb6d83621c5affe141d72c05
Caitie Garrood <caitlinanneg@gmail.com>
2021-02-08 14:44:14 -0500

    assertArraysEqualTest with Mocha

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,3 +1,9 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 
+describe("#assertArraysEqual", () => {
+    it("returns true for [1, 2, 3] and [1, 2, 3]", () => {
+      assert.deepEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]), true);
+    });
+});
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
