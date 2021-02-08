6eef86523a6314afa126d0285c8ea1799ef02001
Caitie Garrood <caitlinanneg@gmail.com>
2021-02-08 14:44:48 -0500

    assertArraysEqual with return values

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,8 +3,10 @@ const eqArrays = require('./eqArrays');
 const assertArraysEqual = function(oneArray, twoArray) {
   if (eqArrays(oneArray, twoArray) === true) {
     console.log('✅✅✅ Assertion Passed');
+    return true;
   } else {
     console.log('❌❌❌ Assertion Failed');
+    return false;
   }
 };
 
