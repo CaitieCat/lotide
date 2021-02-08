4ac4fba0f9939e5851cb66ed9b225e606796c3b8
Caitie Garrood <caitlinanneg@gmail.com>
2021-02-08 13:35:49 -0500

    tail Updated

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 let tail = function(someArray) {
   let newArray = someArray.slice(1);
   return newArray;
